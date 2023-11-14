6577044532c86a07b2e7606012d1eb5a1a5a8bdc
Jay Lokhande <jaylokhande2711@gmail.com>
2023-11-14 18:54:00 +0530

    js

---
+++ static/script.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.sidebar a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            // Prevent the default behavior of the link
+            event.preventDefault();
+
+            // Remove the 'active' class from all links
+            links.forEach(l => l.classList.remove('active'));
+
+            // Add the 'active' class to the clicked link
+            link.classList.add('active');
+
+            // Fetch and display the content for the clicked link
+            const url = link.getAttribute('href');
+            fetchContent(url);
+        });
+    });
+
+    // Function to fetch and display content
+    function fetchContent(url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                const parser = new DOMParser();
+                const newDocument = parser.parseFromString(html, 'text/html');
+
+                // Update the content area with the new content
+                const contentContainer = document.querySelector('.content');
+                contentContainer.innerHTML = newDocument.querySelector('.content').innerHTML;
+            })
+            .catch(error => console.error('Error fetching content:', error));
+    }
+
+
+});
+// ... (previous script content) ...
+
+function showForm(formName) {
+    const formsContainer = document.getElementById('forms-container');
+    fetch(`/${formName}`)
+        .then(response => response.text())
+        .then(html => {
+            formsContainer.innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching form:', error));
+}
+
+// ... (previous script content) ...
+
+function showView(viewName) {
+    const viewContainer = document.getElementById('view-container');
+    fetch(`/${viewName}`)
+        .then(response => response.text())
+        .then(html => {
+            viewContainer.innerHTML = html;
+        })
+        .catch(error => console.error('Error fetching view:', error));
+}
